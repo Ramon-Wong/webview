46a32713ccb2e00d97bb77890d8c21579e73a842
Ramon Wong <rcnwong78@gmail.com>
2025-02-07 14:05:32 -0300

    added some more stuff

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.app;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Build;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.content.pm.ActivityInfo;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,7 +21,35 @@ public class MainActivity extends Activity {
         mWebView = findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        webSettings.setBuiltInZoomControls(false);              // Disable zoom controls
+        webSettings.setDisplayZoomControls(false);              // Hide zoom buttons
+        webSettings.setSupportZoom(false);                      // Disable pinch-to-zoom
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);     // enabling caching
+
+        mWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        mWebView.setWebContentsDebuggingEnabled(true);          // remote debugging
+
         mWebView.setWebViewClient(new MyWebViewClient());
+        mWebView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+
+        WebView.loadUrl("file:///android_asset/your_wasm_file.wasm");
+
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            mWebView.setWebContentsDebuggingEnabled(true);
+        } 
+
+        // For portrait:
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // // For landscape:
+        // setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
 
         // REMOTE RESOURCE
         // mWebView.loadUrl("https://example.com");
